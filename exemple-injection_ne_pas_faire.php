0783a2b3c58c3870f918ec9ce88c6f48425f2346
stephaneG-frep <119919153+stephaneG-frep@users.noreply.github.com>
2024-06-24 10:02:21 +0200

    exemple injection ne pas faire

--- exemple-injection_ne_pas_faire.php
+++ exemple-injection_ne_pas_faire.php
@@ -12,4 +12,15 @@ $result = $query->fetch(PDO::FETCH_ASSOC);
 
 // binValue pour une injection sécurisée
 
+
+/* 
+      A FAIRE
+*/
+$pdo = new PDO('mysql:dbname=bloblog;host=localhost;charset=utf8mb64', 'root', 'root');
+$id = (int)$_GET ['id'];
+$query = $pdo->prepare("SELECT * FROM users WHERE id = $id");
+$query->binValue('id', $id, PDO::PARAM_INT) */
+$query->execute();
+$result = $query->fetch(PDO::FETCH_ASSOC);
+
 ?>
