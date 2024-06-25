1aeed0ec7e839339b2ee7c23bcc3d56f982c508c
stephaneG-frep <119919153+stephaneG-frep@users.noreply.github.com>
2024-06-25 11:42:59 +0200

    debut de admin et connexion

--- lib/menu.php
+++ lib/menu.php
@@ -3,4 +3,5 @@ $mainMenu = [
     'index.php' => ["menu-title" => "Accueil", "head_title" => "Accueil_bloblog_site _d'actus..", "meta_description" =>"Bloblog votre actu a vous !"],
     'actualites.php' => ["menu-title" => "Les Actus", "head_title" => "Toutes les actualitées de_bloblog_site _d'actus..", "meta_description" =>"Toutes les"],
     'a_propos.php' =>["menu-title" => "A propos", "head_title" => "L'histoire de_bloblog_site _d'actus..", "meta_description" =>" L'histoire de Bloblog!"],
- ];
+    'login.php' =>["menu-title" => "connexion", "head_title" => "Connexion a bloblog", "meta_description" =>" Connexion au site ", "exclude" => true],
+];
---
+++ login.php
@@ -0,0 +1,55 @@
+<?php
+require_once __DIR__. "/lib/config.php";
+require_once __DIR__. "/lib/pdo.php";
+require_once __DIR__. "/lib/user.php";
+require_once __DIR__. "/lib/menu.php";
+require_once __DIR__. "/templates/header.php";
+
+$erros = [];
+
+if (isset($_POST['loginUser'])) {   
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $user = verifyUserLoginPassword($pdo, $email, $password);
+    if ($user) {
+       if ($user['role'] === "user") {
+         header("location: index.php");
+       } else {
+        header("location: admin/index.php");
+       }
+    } else {
+       $erros[] = "Email ou mot-de-passe incorrect !!";
+    }
+}
+
+
+
+
+?>
+
+
+<div class="container col-xxl-8 px-4 py-5 ">
+    <h1> Connexion : </h1>
+
+    <?php  foreach($erros as $error) {?>
+    <div class="alert alert-danger">
+        <?=$error?>
+    </div>
+    <?php }?>
+
+    <form action="" method="post">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email : </label>
+            <input type="email" name="email" id="email" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot-de-passe : </label>
+            <input type="password" name="password" id="password" class="form-control" required>
+        </div>
+        <input type="submit" value="connexion" name="loginUser" class="btn btn-primary">
+    </form>
+</div>
+
+<?php require_once __DIR__. "/templates/footer.php"; ?>
